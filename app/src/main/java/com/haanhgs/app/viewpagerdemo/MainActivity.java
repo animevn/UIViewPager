9d6e6e8a5bd1613ec0a3b15466232e8a364dffbc
animevn <haanhth21@gmail.com>
2019-12-22 18:37:21 +0700

    ok

--- app/src/main/java/com/haanhgs/app/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/viewpagerdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.haanhgs.app.viewpagerdemo;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -11,10 +12,28 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.vpMain)
     ViewPager vpMain;
 
+    private int[] imageList;
+
+    private void initImageList(){
+        TypedArray imgStrings = getResources().obtainTypedArray(R.array.imgList);
+        imageList = new int[imgStrings.length()];
+        for (int i = 0; i < imgStrings.length(); i++){
+            imageList[i] = imgStrings.getResourceId(i, 0);
+        }
+        imgStrings.recycle();
+    }
+
+    private void initViewPager(){
+        Adapter adapter = new Adapter(imageList);
+        vpMain.setAdapter(adapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initImageList();
+        initViewPager();
     }
 }
