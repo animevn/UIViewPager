695a26e53d2c6d6cf3e4b5f2e1bc25ce34e3e516
animevn <haanhth21@gmail.com>
2019-12-22 18:30:43 +0700

    add adapter

---
+++ app/src/main/java/com/haanhgs/app/viewpagerdemo/Adapter.java
@@ -0,0 +1,44 @@
+package com.haanhgs.app.viewpagerdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class Adapter extends PagerAdapter {
+
+    private final int[] imageList;
+
+    public Adapter(int[] imageList){
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        Context context = container.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.viewpager, container, false);
+        ImageView ivPager = view.findViewById(R.id.ivPager);
+        ivPager.setImageResource(imageList[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return imageList.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/haanhgs/app/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/viewpagerdemo/MainActivity.java
@@ -1,14 +1,20 @@
 package com.haanhgs.app.viewpagerdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.vpMain)
+    ViewPager vpMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
